28a2cc5413d44e453e26d7820c04c33a6628ddb5
YUTA917 <119489307+YUTA917@users.noreply.github.com>
2024-05-27 18:36:07 +0900

    port変更

--- soloPJT/src/router.js
+++ soloPJT/src/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./db");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(
