989887f44af8b10f7b20975e1e2bb9113d33fcb5
YUTA917 <119489307+YUTA917@users.noreply.github.com>
2024-05-28 00:06:00 +0900

    表示変更

---
+++ soloPJT/src/DisplayPark.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function DisplayPark(props) {
+	const { parks } = props;
+	return parks.map((ele, index) => (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardMedia
+				component="img"
+				alt={ele.park_name}
+				height="140"
+				image={`./image/${ele.park_name}.jpeg`}
+			/>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="div">
+					{ele.park_name}
+				</Typography>
+				<Typography variant="body2" color="text.secondary">
+					{ele.prefecture_name}
+					{ele.city_name}
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button size="small">Share</Button>
+				<Button size="small">Learn More</Button>
+			</CardActions>
+		</Card>
+	));
+}
