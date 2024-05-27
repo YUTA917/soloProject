3fd09545dd23622770d52524c3aebb8141a415c3
YUTA917 <119489307+YUTA917@users.noreply.github.com>
2024-05-27 18:22:36 +0900

    knexfile変更

--- soloPJT/src/db/knexfile.js
+++ soloPJT/src/db/knexfile.js
@@ -26,11 +26,8 @@ module.exports = {
 
 	production: {
 		client: "postgresql",
-		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
-		},
+		connection: process.env.DATABASE_URL,
+
 		pool: {
 			min: 2,
 			max: 10,
