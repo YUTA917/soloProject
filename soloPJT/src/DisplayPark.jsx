d4b7d376726839369a98c0cd59409032a4b2ee98
YUTA917 <119489307+YUTA917@users.noreply.github.com>
2024-05-28 09:26:21 +0900

    buildなし

--- soloPJT/src/DisplayPark.jsx
+++ soloPJT/src/DisplayPark.jsx
@@ -9,6 +9,7 @@ import Grid from "@mui/material/Grid";
 import image1 from "./image/鶴舞公園.jpeg";
 import image2 from "./image/明石公園.jpeg";
 import image3 from "./image/安満遺跡公園.jpeg";
+import { reverse } from "lodash";
 
 export default function DisplayPark(props) {
 	const { parks } = props;
@@ -31,46 +32,52 @@ export default function DisplayPark(props) {
 	// 	);
 	// }
 
-	return parks.map((ele, index) => (
-		<Grid container alignItems="center" justify="center">
-			<Grid item xs={15}>
-				<Card
-					sx={{
-						maxWidth: 345,
-						width: "80%",
-						height: "auto",
-						margin: 5,
-						borderRadius: 5,
-					}}
-					style={{
-						margin: "auto",
-					}}
-				>
-					<CardMedia
-						component="img"
-						alt={ele.park_name}
-						height="140"
-						// src={dummy}
-						// image={`./public/image/${ele.park_name}.jpeg`}
-						image={image1}
-					/>
-					{/* {getImage(ele.park_name)} */}
+	return parks
+		.map((ele, index) => (
+			<Grid container alignItems="center" justify="center">
+				<Grid item xs={15}>
+					<Card
+						sx={{
+							maxWidth: 345,
+							width: "80%",
+							height: "auto",
+							margin: 5,
+							borderRadius: 5,
+						}}
+						style={{
+							margin: "auto",
+						}}
+					>
+						<CardMedia
+							component="img"
+							alt={ele.park_name}
+							height="140"
+							// src={dummy}
+							// image={`./public/image/${ele.park_name}.jpeg`}
+							image={image1}
+						/>
+						{/* {getImage(ele.park_name)} */}
 
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="div">
-							{ele.park_name}
-						</Typography>
-						<Typography variant="body2" color="text.secondary">
-							{ele.prefecture_name}
-							{ele.city_name}
-						</Typography>
-					</CardContent>
-					<CardActions>
-						<Button size="small">Share</Button>
-						<Button size="small">Learn More</Button>
-					</CardActions>
-				</Card>
+						<CardContent>
+							<Typography
+								gutterBottom
+								variant="h5"
+								component="div"
+							>
+								{ele.park_name}
+							</Typography>
+							<Typography variant="body2" color="text.secondary">
+								{ele.prefecture_name}
+								{ele.city_name}
+							</Typography>
+						</CardContent>
+						<CardActions>
+							<Button size="small">Share</Button>
+							<Button size="small">Learn More</Button>
+						</CardActions>
+					</Card>
+				</Grid>
 			</Grid>
-		</Grid>
-	));
+		))
+		.reverse();
 }
