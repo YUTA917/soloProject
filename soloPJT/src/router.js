e5eb2da5f6ba3618b8d13d3a94addceaa0136c95
YUTA917 <119489307+YUTA917@users.noreply.github.com>
2024-05-28 09:57:29 +0900

    appuse変更

--- soloPJT/src/router.js
+++ soloPJT/src/router.js
@@ -4,7 +4,7 @@ const db = require("./db");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use("/", express.static("/", express.static("dist")));
+app.use("/", express.static("dist"));
 app.use(express.json());
 app.use(
 	express.urlencoded({
